298e4a93c93b5249230489a9a75905df69ca31b1
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-09 11:52:24 -0600

    Practicing using Arrays

---
+++ Arrays/arrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int A[5];
+    A[0] = 12;
+    A[1] = 15;
+    A[2] = 25;
+
+    cout << sizeof(A);
+    cout << A[1];
+    printf("/%d\n", A[2]);
+
+    return 0;
+}
