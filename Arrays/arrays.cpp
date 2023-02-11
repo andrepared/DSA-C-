d352622e7c0b2fab184aaca198178a31bbefe8c5
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-10 20:45:10 -0600

    Java Practice

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -7,10 +7,10 @@ int main()
 {
     int A[10] = {1,2,3,4,5,6};
 
-    for(int i = 0; i<10; i++)
+    for(int x:A)
     {
-        cout<<A[i]<<endl;
-        
+        cout<<x<<endl;
+
     }
 
     return 0;
