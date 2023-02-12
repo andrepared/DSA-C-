a20d73f474ab84aeeae407778305572f14e46d29
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-02-12 12:46:03 -0600

    ADT

--- Arrays/arrayADT.cpp
+++ Arrays/arrayADT.cpp
@@ -3,7 +3,7 @@
 
 struct Array 
 {
-    int *A;
+    int A[20];
     int size;
     int length;
 };
@@ -12,24 +12,12 @@ struct Array
         int i;
         printf("\nElements are\n");
         for (i = 0; i < arr.length; i++);
-        printf("%d", arr.A[i]);
+        printf("%d" ,arr.A[i]);
 }
     int main()
     {
-    struct Array arr;
-    int n, i;
-    printf("Enter size of an array ");
-    scanf("%d",&arr.size);
-    arr.A = (int *)malloc(arr.size*sizeof(int));
-    arr.length = 0;
-
-    printf("Enter number of numbers ");
-    scanf("%d",&n);
-    printf("Enter all elements \n");
-    for (i = 0; i < n; i++)
+        struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
     
-        scanf("%d", &arr.A[i]);
-        arr.length = n;
         Display(arr);
         return 0;
     }
